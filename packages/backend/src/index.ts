a6ae7add74b940004632dfc7ed759c9687d15485
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-07-18 15:57:52 +0900

    delete unused endpoint

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,13 +4,6 @@ import cors from "@elysiajs/cors"
 const app = new Elysia()
   .use(cors())
   .get("/", () => "Hi Elysia")
-  .get("/id/:id", ({ params: { id }}) => id)
-  .post("/mirror", ({ body }) => body, {
-    body: t.Object({
-      id: t.Number(),
-      name: t.String()
-    })
-  })
   .post("/name", ({ body }) => body, {
     body: t.Object({
       name: t.String({ minLength: 5 })
